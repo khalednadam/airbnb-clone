05740dd780ddd90fcbaef35afbc6bf407928ec5d
Khaled Nadam <khalednadam@gmail.com>
2023-09-01 00:27:01 +0300

    add categories and fix style

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from "../Container";
 import Logo from "../Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "../Categories";
 
 interface NavbarProps {
     currentUser?: User | null
@@ -29,6 +30,7 @@ export const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
 		</div>
 	);
 };
